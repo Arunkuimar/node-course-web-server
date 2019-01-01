9315cf3678822b90885b9cf60ac0f2ec9529086f
Arunkuimar <arunachaalamm@gmail.com>
2019-01-01 19:47:32 +0530

    Initial commit

---
+++ async-basics.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+// app.get('/',(req,res)=>{
+//     //res.send('<h1>hai</h1>');
+//     // res.send({
+//     //     name:'Arun',
+//     //     emp_id:'1272'
+//     // });
+//     res.send('index.html')
+// });
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+//     next();
+// })
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        a:'hai dude'
+    });
+});
+app.use(express.static(__dirname));
+app.listen(3000);
